f8620b22cae384f8448de9f5065ef45b0feda388
Murillo Vellasco <murillovellasco@gmail.com>
2025-08-14 14:09:07 +0200

    Add comparison of C1 and dZh values between HEPfit and IDM

--- setup_scripts/create_higgs_venv_py38.sh
+++ setup_scripts/create_higgs_venv_py38.sh
@@ -8,11 +8,11 @@ module load miniforge/4.9.2-7-py38
 python -m venv higgs_venv_py38
 source ./higgs_venv_py38/bin/activate
 python -m pip install numpy==1.24 # pyCollier seems to be incompatible with numpy 2.0+
-python -m pip install scipy pandas tqdm matplotlib
+python -m pip install scipy pandas tqdm matplotlib iminuit
 python -m pip install ipykernel ipywidgets
 
 # export CMAKE_ARGS="-DCMAKE_POLICY_VERSION_MINIMUM=3.5" # issue with cmake and pyCollier
-python -m pip install pyCollier
+python -m pip install pyCollier anyBSM
 
 # cd /cephfs/user/mrebuzzi/phd/HiggsTools/future_projections/higgstools/
 # pip install .
